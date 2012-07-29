ccbbe142bee4ba963536023b7ee8d9e1bc4a1ce9
Simon Norberg <simon@pthread.se>
2012-07-29 23:00:19 +0200

    first hardcoded support for [ and ]

--- compiler.hs
+++ compiler.hs
@@ -17,13 +17,20 @@ translate :: Char -> Int -> (String, Int)
 translate '>' temp = (unlines[ printf "%%%d = load i8** %%ptr" temp,
 	printf "%%%d = getelementptr inbounds i8* %%%d, i32 1" (temp+1) temp,
 	printf "store i8* %%%d, i8** %%ptr" (temp+1)], temp+2)
-translate '<' temp  = ("ptr--", temp+0)
+--ptr--
+translate '<' temp = (unlines[ printf "%%%d = load i8** %%ptr" temp,
+	printf "%%%d = getelementptr inbounds i8* %%%d, i32 -1" (temp+1) temp,
+	printf "store i8* %%%d, i8** %%ptr" (temp+1)], temp+2)
 --(*ptr)++
 translate '+' temp = (unlines[printf "%%%d = load i8** %%ptr" temp,
 	printf "%%%d = load i8* %%%d" (temp+1) temp,
 	printf "%%%d = add nsw i8 %%%d, 1" (temp+2) (temp+1),
 	printf "store i8 %%%d, i8* %%%d" (temp+2) temp], temp+3)
-translate '-' temp = ("(*ptr)--",  temp+0)
+--(*ptr)--
+translate '-' temp = (unlines[printf "%%%d = load i8** %%ptr" temp,
+	printf "%%%d = load i8* %%%d" (temp+1) temp,
+	printf "%%%d = sub nsw i8 %%%d, 1" (temp+2) (temp+1),
+	printf "store i8 %%%d, i8* %%%d" (temp+2) temp], temp+3)
 --putchar(*ptr)
 translate '.' temp = (unlines [printf "%%%d = load i8** %%ptr" temp,
 	printf "%%%d = load i8* %%%d" (temp+1) temp,
@@ -31,11 +38,15 @@ translate '.' temp = (unlines [printf "%%%d = load i8** %%ptr" temp,
 --(*ptr) = getchar()
 translate ',' temp = ("(*ptr) = getchar()", temp+0)
 --while (*ptr){
-translate '[' temp = ("while (*ptr){", temp+0)
+translate '[' temp = (unlines [printf "br label %%%d" temp,
+	printf "%%%d = load i8** %%ptr" (temp+1),
+	printf "%%%d = load i8* %%%d" (temp+2) (temp+1),
+	printf "%%%d = icmp eq i8 %%%d, 0" (temp+3) (temp+2),
+	printf "br i1 %%%d, label %%82, label %%%d" (temp+3) (temp+4)], temp+5)
 --}
-translate ']' temp = ("}", temp+0)
+translate ']' temp = (printf "br label %%18", temp+1)
 translate c temp = ("", temp+0)	
 
 main = do  
-    contents <- readFile "test/42.bf"  
+    contents <- readFile "test/42_looping.bf"  
     compile contents 
