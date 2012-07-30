151c6f4c38065b74301a6a37566fe47538aa670a
Simon Norberg <simon@pthread.se>
2012-07-30 11:10:55 +0200

    added proper support from [ and ]
    HelloWorld.bf and 42_looping.bf compiles and run correctly :)

--- compiler.hs
+++ compiler.hs
@@ -1,52 +1,58 @@
 import Text.Printf
 
 compile str = do
-	let code = rec str 0
+	let code = rec str 0 []
 	writeFile "test/helloworld.ll" (unlines code)
 
-rec :: String -> Int -> [String]
-rec str temp
+rec :: String -> Int -> [Int]-> [String]
+rec str temp label
 	| str == [] = []
 	| otherwise = code where
-	(code1, nextTemp) = translate (head str) temp
-	codeN = (rec (tail str) nextTemp)
+	(code1, nextTemp, nextLabel) = translate (head str) temp label
+	codeN = (rec (tail str) nextTemp nextLabel)
 	code = [code1] ++ codeN
 
 --ptr++
-translate :: Char -> Int -> (String, Int)	
-translate '>' temp = (unlines[ printf "%%%d = load i8** %%ptr" temp,
+translate :: Char -> Int -> [Int] -> (String, Int, [Int])	
+translate '>' temp labels = (unlines[ printf "%%%d = load i8** %%ptr" temp,
 	printf "%%%d = getelementptr inbounds i8* %%%d, i32 1" (temp+1) temp,
-	printf "store i8* %%%d, i8** %%ptr" (temp+1)], temp+2)
+	printf "store i8* %%%d, i8** %%ptr" (temp+1)], temp+2, labels)
 --ptr--
-translate '<' temp = (unlines[ printf "%%%d = load i8** %%ptr" temp,
+translate '<' temp labels = (unlines[ printf "%%%d = load i8** %%ptr" temp,
 	printf "%%%d = getelementptr inbounds i8* %%%d, i32 -1" (temp+1) temp,
-	printf "store i8* %%%d, i8** %%ptr" (temp+1)], temp+2)
+	printf "store i8* %%%d, i8** %%ptr" (temp+1)], temp+2, labels)
 --(*ptr)++
-translate '+' temp = (unlines[printf "%%%d = load i8** %%ptr" temp,
+translate '+' temp labels = (unlines[printf "%%%d = load i8** %%ptr" temp,
 	printf "%%%d = load i8* %%%d" (temp+1) temp,
 	printf "%%%d = add nsw i8 %%%d, 1" (temp+2) (temp+1),
-	printf "store i8 %%%d, i8* %%%d" (temp+2) temp], temp+3)
+	printf "store i8 %%%d, i8* %%%d" (temp+2) temp], temp+3, labels)
 --(*ptr)--
-translate '-' temp = (unlines[printf "%%%d = load i8** %%ptr" temp,
+translate '-' temp labels = (unlines[printf "%%%d = load i8** %%ptr" temp,
 	printf "%%%d = load i8* %%%d" (temp+1) temp,
 	printf "%%%d = sub nsw i8 %%%d, 1" (temp+2) (temp+1),
-	printf "store i8 %%%d, i8* %%%d" (temp+2) temp], temp+3)
+	printf "store i8 %%%d, i8* %%%d" (temp+2) temp], temp+3, labels)
 --putchar(*ptr)
-translate '.' temp = (unlines [printf "%%%d = load i8** %%ptr" temp,
+translate '.' temp labels = (unlines [printf "%%%d = load i8** %%ptr" temp,
 	printf "%%%d = load i8* %%%d" (temp+1) temp,
-	printf "%%%d = call i32 @putchar(i8 %%%d)" (temp+2) (temp+1)], temp+3)
+	printf "%%%d = call i32 @putchar(i8 %%%d)" (temp+2) (temp+1)], temp+3, labels)
 --(*ptr) = getchar()
-translate ',' temp = ("(*ptr) = getchar()", temp+0)
+translate ',' temp labels = ("(*ptr) = getchar()", temp+0, labels)
 --while (*ptr){
-translate '[' temp = (unlines [printf "br label %%%d" temp,
-	printf "%%%d = load i8** %%ptr" (temp+1),
-	printf "%%%d = load i8* %%%d" (temp+2) (temp+1),
-	printf "%%%d = icmp eq i8 %%%d, 0" (temp+3) (temp+2),
-	printf "br i1 %%%d, label %%82, label %%%d" (temp+3) (temp+4)], temp+5)
+translate '[' temp labels = (unlines [printf "br label %%beginLoop%d" temp,
+	printf "beginLoop%d:" temp,
+	printf "%%%d = load i8** %%ptr" (temp),
+	printf "%%%d = load i8* %%%d" (temp+1) (temp),
+	printf "%%%d = icmp eq i8 %%%d, 0" (temp+2) (temp+1),
+	printf "br i1 %%%d, label %%endLoop%d, label %%%d" (temp+2) temp (temp+3)], temp+4, labels ++ [temp])
 --}
-translate ']' temp = (printf "br label %%18", temp+1)
-translate c temp = ("", temp+0)	
+-- fundera ut hur labels ska genereras. behöver [ peka på ] samtidigt som ] pekar på [ ???
+-- 
+translate ']' temp labels = (unlines [printf "br label %%beginLoop%d" (last labels),
+	printf "br label %%endLoop%d" (last labels),
+	printf  "endLoop%d:" (last labels)],temp+1, init labels)
+
+translate c temp labels = ("", temp+0, labels)	
 
 main = do  
-    contents <- readFile "test/42_looping.bf"  
+    contents <- readFile "test/helloworld.bf"  
     compile contents 
